b4c7fc6a562af98edb5cb15500c6f9d37fc2889d
thomasinfor <thomaswang2003@gmail.com>
2020-06-10 15:12:03 +0800

    change

--- research/project.js
+++ research/project.js
@@ -5,31 +5,29 @@ function people(data){
 }
 function project(data){
   return `
-    <div id="information">
-      <div class="project" id="${data.title}">
-        <div class="project-text">
-          <h1 class="header">${data.title}</h1>
-          <p style="text-align: right;">${people(data)}</p>
-          ${data.intro.replace('\n','<br>')}
-        </div>
-        <div>
-          <h4>
-            <div class="w3-button w3-round-large w3-border w3-border-teal pdfbutton" onclick="pdfbutton(this);">Show PDF</div><div class="w3-dropdown-hover">
-              <div class="w3-button w3-round-large w3-border w3-border-teal pdfbutton">Download</div>
-              <div class="w3-dropdown-content w3-bar-block w3-card-4">
-                <a class="w3-bar-item w3-button" href="./files/${data.title}_作品說明書.pdf" download="${data.title}_作品說明書.pdf">作品說明書</a>
-                <a class="w3-bar-item w3-button" href="./files/${data.title}_海報.pdf" download="${data.title}_海報.pdf">海報</a>
-              </div>
+    <div class="project" id="${data.title}">
+      <div class="project-text">
+        <h1 class="header">${data.title}</h1>
+        <p style="text-align: right;">${people(data)}</p>
+        ${data.intro.replace('\n','<br>')}
+      </div>
+      <div>
+        <h4>
+          <div class="w3-button w3-round-large w3-border w3-border-teal pdfbutton" onclick="pdfbutton(this);">Show PDF</div><div class="w3-dropdown-hover">
+            <div class="w3-button w3-round-large w3-border w3-border-teal pdfbutton">Download</div>
+            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+              <a class="w3-bar-item w3-button" href="./files/${data.title}_作品說明書.pdf" download="${data.title}_作品說明書.pdf">作品說明書</a>
+              <a class="w3-bar-item w3-button" href="./files/${data.title}_海報.pdf" download="${data.title}_海報.pdf">海報</a>
             </div>
-          </h4>
-          <div class="project-file">
-            <div class="w3-row">
-              <div class="w3-col s12 m12 l6">
-                <iframe src="./files/${data.title}research.pdf#toolbar=0" width="100%" height="400px"></iframe>
-              </div>
-              <div class="w3-col s12 m12 l6">
-                <iframe src="./files/${data.title}_展板.pdf#toolbar=0" width="100%" height="400px"></iframe>
-              </div>
+          </div>
+        </h4>
+        <div class="project-file">
+          <div class="w3-row">
+            <div class="w3-col s12 m12 l6">
+              <iframe src="./files/${data.title}research.pdf#toolbar=0" width="100%" height="400px"></iframe>
+            </div>
+            <div class="w3-col s12 m12 l6">
+              <iframe src="./files/${data.title}_展板.pdf#toolbar=0" width="100%" height="400px"></iframe>
             </div>
           </div>
         </div>
@@ -46,6 +44,18 @@ var example=[
         title: "以人工智慧技術開發自動配音效系統",
         intro: `<h5 class="intro">　　近日網路發達，影片的流通更為便利，但為影片加上適當的音效往往需要耗費許多人力與時間資源。本研究旨在研究利用人工智慧深度網路技術訓練模型，並期望訓練後的模型能為當下影片的內容配上適當的音效，使得幫影片配音的過程可以加速，並減少人力需求。本研究主要利用 Keras 這個 Python 套件創建模型，並參考 ResNet 這個著名的深度學習模型，透過大量已標記圖片當作訓練資料，並進一步修改模型的架構，使準確率得到提升。研究結果符合預期，能在適合的類別中為影片配上合理的音效。</h5>`
       },
+      {
+        member: "王勻 林尚廷 蕭梓宏",
+        professor: "陽明大學 陳麗芬教授",
+        title: "aa",
+        intro: `jizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz`
+      },
+      {
+        member: "王勻 林尚廷 蕭梓宏",
+        professor: "陽明大學 陳麗芬教授",
+        title: "bb",
+        intro: `jizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz`
+      }
     ]
   },
   {
@@ -54,17 +64,29 @@ var example=[
       {
         member: "王勻 林尚廷 蕭梓宏",
         title: "jizz",
-        intro: `jizz<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>`
+        intro: `jizz`
       },
+    ]
+  },
+  {
+    subject: "physics",
+    projects: [
+      {
+        member: "李秉叡",
+        professor: "叛逆秉叡",
+        title: "反氣泡秉叡",
+        intro: "我就是不做專題 爽啦"
+      }
     ]
   }
 ];
 function process(data){
   for(var i of data){
     var content='',btnlst='';
-    for(var j of i.projects)
+    for(var j of i.projects){
       content+=project(j);
       btnlst+=`<a class="w3-bar-item w3-button" onclick="show(this.text);toggle(lst);">${j.title}</a>`;
+    }
     document.getElementById(i.subject).innerHTML=content;
     document.getElementById(i.subject+'-btn').innerHTML=btnlst;
   }
