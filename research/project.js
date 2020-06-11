cc8ea8bdb57d3aaf58ee8a23298bb954c270feb5
thomasinfor <thomaswang2003@gmail.com>
2020-06-11 22:17:34 +0800

    jizz

--- research/project.js
+++ research/project.js
@@ -12,62 +12,25 @@ function project(data){
         ${data.intro.replace('\n','<br>')}
       </div>
       <div>
-        <h4>
-          <div class="w3-button w3-round-large w3-border w3-border-teal pdfbutton" onclick="pdfbutton(this);">Show PDF</div><div class="w3-dropdown-hover">
-            <div class="w3-button w3-round-large w3-border w3-border-teal pdfbutton">Download</div>
-            <div class="w3-dropdown-content w3-bar-block w3-card-4">
-              <a class="w3-bar-item w3-button" href="./files/${data.title}_作品說明書.pdf" download="${data.title}_作品說明書.pdf">作品說明書</a>
-              <a class="w3-bar-item w3-button" href="./files/${data.title}_海報.pdf" download="${data.title}_海報.pdf">海報</a>
-            </div>
-          </div>
-        </h4>
-        <div class="project-file">
-          <div class="w3-row">
-            <div class="w3-col s12 m12 l6">
-              <iframe src="./files/${data.title}research.pdf#toolbar=0" width="100%" height="400px"></iframe>
-            </div>
-            <div class="w3-col s12 m12 l6">
-              <iframe src="./files/${data.title}_展板.pdf#toolbar=0" width="100%" height="400px"></iframe>
-            </div>
-          </div>
+        <div class="w3-button w3-round-large w3-border w3-border-teal pdfbutton" onclick="pdfbutton(this);">Show PDF</div>
+        <div class="project-file" style="display: none;">
+          ${data.ppt}
         </div>
       </div>
     </div>`;
 }
 var example=[
   {
-    subject: "information",
+    subject: "math",
     intro: "this is an intriduction to this subject.",
     projects: [
       {
-        member: "王勻 林尚廷 蕭梓宏",
-        professor: "陽明大學 陳麗芬教授",
-        title: "以人工智慧技術開發自動配音效系統",
-        intro: `<h5 class="intro">　　近日網路發達，影片的流通更為便利，但為影片加上適當的音效往往需要耗費許多人力與時間資源。本研究旨在研究利用人工智慧深度網路技術訓練模型，並期望訓練後的模型能為當下影片的內容配上適當的音效，使得幫影片配音的過程可以加速，並減少人力需求。本研究主要利用 Keras 這個 Python 套件創建模型，並參考 ResNet 這個著名的深度學習模型，透過大量已標記圖片當作訓練資料，並進一步修改模型的架構，使準確率得到提升。研究結果符合預期，能在適合的類別中為影片配上合理的音效。</h5>`
-      },
-      {
-        member: "王勻 林尚廷 蕭梓宏",
-        professor: "陽明大學 陳麗芬教授",
-        title: "aa",
-        intro: `jizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz`
-      },
-      {
-        member: "王勻 林尚廷 蕭梓宏",
-        professor: "陽明大學 陳麗芬教授",
-        title: "bb",
-        intro: `jizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz`
+        member: "yoda",
+        professor: "master yoda",
+        title: "math-math",
+        intro: "may the force be with you",
+        ppt: `<iframe src="https://onedrive.live.com/embed?cid=24AC57FC109740EC&amp;resid=24AC57FC109740EC%219447&amp;authkey=AETD8EojVCuH23k&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0" style="width: 90vw;height: 54vw;max-width: 600px;max-height: 360px;"></iframe>`
       }
-    ]
-  },
-  {
-    subject: "chemistry",
-    intro: "this is an intriduction to this subject.",
-    projects: [
-      {
-        member: "王勻 林尚廷 蕭梓宏",
-        title: "jizz",
-        intro: `jizz`
-      },
     ]
   },
   {
@@ -78,7 +41,51 @@ var example=[
         member: "李秉叡",
         professor: "叛逆秉叡",
         title: "反氣泡秉叡",
-        intro: "我就是不做專題 爽啦"
+        intro: "我就是不做專題 爽啦",
+        ppt: `<iframe src="https://onedrive.live.com/embed?cid=24AC57FC109740EC&amp;resid=24AC57FC109740EC%219447&amp;authkey=AETD8EojVCuH23k&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0" style="width: 90vw;height: 54vw;max-width: 600px;max-height: 360px;"></iframe>`
+      }
+    ]
+  },
+  {
+    subject: "chemistry",
+    intro: "this is an intriduction to this subject.",
+    projects: [
+      {
+        member: "王勻 林尚廷 蕭梓宏",
+        title: "chemistry-chemistry",
+        intro: `jizz`,
+        ppt: `<iframe src="https://onedrive.live.com/embed?cid=24AC57FC109740EC&amp;resid=24AC57FC109740EC%219447&amp;authkey=AETD8EojVCuH23k&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0" style="width: 90vw;height: 54vw;max-width: 600px;max-height: 360px;"></iframe>`
+      }
+    ]
+  },
+  {
+    subject: "biology",
+    intro: "this is an intriduction to this subject.",
+    projects: [
+      {
+        member: "王勻 林尚廷 蕭梓宏",
+        title: "biology-biology",
+        intro: `豬哥啊`,
+        ppt: `<iframe src="https://onedrive.live.com/embed?cid=24AC57FC109740EC&amp;resid=24AC57FC109740EC%219447&amp;authkey=AETD8EojVCuH23k&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0" style="width: 90vw;height: 54vw;max-width: 600px;max-height: 360px;"></iframe>`
+      }
+    ]
+  },
+  {
+    subject: "information",
+    intro: "this is an intriduction to this subject.",
+    projects: [
+      {
+        member: "王勻 林尚廷 蕭梓宏",
+        professor: "陽明大學 陳麗芬教授",
+        title: "以人工智慧技術開發自動配音效系統",
+        intro: `<h5 class="intro">　　近日網路發達，影片的流通更為便利，但為影片加上適當的音效往往需要耗費許多人力與時間資源。本研究旨在研究利用人工智慧深度網路技術訓練模型，並期望訓練後的模型能為當下影片的內容配上適當的音效，使得幫影片配音的過程可以加速，並減少人力需求。本研究主要利用 Keras 這個 Python 套件創建模型，並參考 ResNet 這個著名的深度學習模型，透過大量已標記圖片當作訓練資料，並進一步修改模型的架構，使準確率得到提升。研究結果符合預期，能在適合的類別中為影片配上合理的音效。</h5>`,
+        ppt: `<iframe src="https://onedrive.live.com/embed?cid=24AC57FC109740EC&amp;resid=24AC57FC109740EC%219447&amp;authkey=AETD8EojVCuH23k&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0" style="width: 90vw;height: 54vw;max-width: 600px;max-height: 360px;"></iframe>`
+      },
+      {
+        member: "王勻 林尚廷 蕭梓宏",
+        title: "TIOJ-INFOR ONLINE JUDGE",
+        intro: `jizz`,
+        ppt: `<iframe src="https://onedrive.live.com/embed?cid=24AC57FC109740EC&amp;resid=24AC57FC109740EC%219447&amp;authkey=AETD8EojVCuH23k&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0" style="width: 90vw;height: 54vw;max-width: 600px;max-height: 360px;"></iframe>`
       }
     ]
   }
